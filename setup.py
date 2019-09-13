03892f45f534994a0206e40a59fee513dd57c746
gpenverne <gpenverne@gmail.com>
2019-09-13 16:29:38 +0200

    :construction: wip

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="random-game-name-generator",
+    name="rgng",
     version="0.0.1",
     author="enriqueav",
     author_email="",
